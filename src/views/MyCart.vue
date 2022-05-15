76069b7a866590f105e74cc323254f5d7833f364
Senshiben-efrei <ayman.ben.hajjaj@efrei.net>
2022-05-15 23:48:14 +0200

    added link to git repo

--- src/views/MyCart.vue
+++ src/views/MyCart.vue
@@ -2,6 +2,9 @@
     <h1>Mon panier</h1>
     <img src="../assets/search.svg" alt="" style="width: 300px; padding: 50px;">
     <h1>En cours de maintenance</h1>
+    <a href="https://github.com/Senshiben-efrei/pageone/tree/ongoing-senshiben" target="blank">
+        <h3 style="color: #7f5af0">Cliquer suivre l'avancement du projet sur Github</h3>
+    </a>
 </template>
 
 <script>
